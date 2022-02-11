a24404c118d09cf44193886dbc750173b53431fb
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-10 19:15:34 -0500

    Init

---
+++ Pets.py
@@ -0,0 +1,29 @@
+# PETS
+
+class Pet:
+
+    def __init__ (self, n = "BLANK_PET"):
+        self.name = n
+        self.type
+        self.tricks
+        self.health
+        self.energy
+
+    def sleep():
+        pass
+
+    def eat():
+        pass
+
+    def play():
+        pass
+
+    def noise():
+        pass
+
+
+
+
+
+
+######################
